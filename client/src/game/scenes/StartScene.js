aa1c710cef66ef5cd5881e1b544eae75d4c4c912
timealiver <timealive@mail.ru>
2024-05-06 23:25:12 +0300

    Hotfix

--- client/src/game/scenes/StartScene.js
+++ client/src/game/scenes/StartScene.js
@@ -16,6 +16,7 @@ export class StartScene extends Scene {
     this.cameras.main.setBackgroundColor(0x30f0f0);
     this.matter.world.setBounds(0,0,width,height,2,1,1,1,1); // Создаем мир Matter
     this.player = new Player(this,width-500,height/2); // Создаем gameObject
+    this.add.image(width/2, height/2, "room");
 
     this.door = this.add.sprite(width-50, height/2, 'door'); // Создаем объект двери
     this.doorHitbox = this.matter.add.rectangle(this.door.x, this.door.y, this.door.width, this.door.height, { isSensor: true });
@@ -23,6 +24,28 @@ export class StartScene extends Scene {
     this.matter.world.add(this.doorHitbox);
     this.matter.world.add(this.player.body);
     this.matter.collision.create(this.player,this.doorHitbox);
+
+    this.matter.collision.create(this.player,this.doorHitbox);
+    var fullText = 'Ночь. Крыши частных секторов.';
+    var text = '';
+    var textObject = this.add.text(width/2-fullText.length-150,height/6, "Button",{fontSize: "30px",fontFamily:"Undertale"})
+    var typingTimer = this.time.addEvent({
+      delay: 100,
+      loop: true,
+      callback: function() {
+          text += fullText.substring(0, 1);
+          fullText = fullText.substring(1);
+          textObject.setText(text);
+
+          if (fullText.length === 0) {
+              typingTimer.destroy();
+          }
+      },
+      callbackScope: this
+  });
+    //let t= new TextBox(this,100,width/2-fullText.length,height/6,"РААА",true)
+    //t.typing();
+
     EventBus.emit('current-scene-ready', this)
   }
 
